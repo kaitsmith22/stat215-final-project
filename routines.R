2be18a20561747f541d0ea76329f79c0d21152fc
Ahmed Eldeeb <a.eldeeb@gail.com>
2022-12-05 12:00:35 -0800

    stylistic changes to roc curve plots

--- routines.R
+++ routines.R
@@ -14,7 +14,24 @@ metrics <- function(truth, classPreds){
 
 rocs <- function(truth, numPreds, classPreds, baseline1, baseline2){
   library(pROC)
-  roc(as.numeric(truth), numPreds, plot=T, lty=3)
+  roc(as.numeric(truth), numPreds, plot=T, lty=1, lwd=2)
+  
+  m <- metrics(truth, classPreds)
+  points(m[2,1], m[1,1], pch=16)
+  abline(v=m[2,1], lty=2)
+  abline(h=m[1,1], lty=2)
+  
+  m <- metrics(truth, baseline1)
+  points(m[2,1], m[1,1], pch=18)
+  m <- metrics(truth, baseline2)
+  points(m[2,1], m[1,1], pch=18)
+  
+  grid()
+}
+
+rocs2 <- function(truth, numPreds, classPreds, baseline1, baseline2){
+  library(pROC)
+  roc(as.numeric(truth), numPreds, ci=T, plot=T, lty=3)
   roc(as.numeric(truth), classPreds, plot=T, add=T)
   roc(as.numeric(truth), baseline1, plot=T, add=T)
   roc(as.numeric(truth), baseline2, plot=T, add=T)
